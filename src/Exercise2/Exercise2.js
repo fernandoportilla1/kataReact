9e6e8aa06aed0f53fdd8c81912652fbe33d47a13
rojoyin <rojoyin@gmail.com>
2019-08-31 16:35:30 -0500

    Add ex2 solution

--- src/Exercise2/Exercise2.js
+++ src/Exercise2/Exercise2.js
@@ -1,20 +1,25 @@
 import React, {Component} from 'react';
 
+const ALPHABET = 'abcdefghijklmnopqrstuvwxyz';
 class Exercise2 extends Component {
   constructor(props) {
     super(props);
-    this.state = {value: ''};
+    this.state = {value: '', message: ''};
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
-  
+
   render() {
+    const {message} = this.state;
     return (
       <div className="container">
         <form onSubmit={this.handleSubmit}>
           <input type="text" value={this.state.value} onChange={this.handleChange} />
           <input type="submit" value="Submit" />
         </form>
+        <div>
+          Mensaje original: {message}
+        </div>
       </div>
     );
   }
@@ -24,8 +29,34 @@ class Exercise2 extends Component {
   }
 
   handleSubmit(event) {
-    console.log('A name was submitted: ' + this.state.value);
+    const {value} = this.state;
+    const params = value.split(':');
+    const rowNumber = Number(params[0]);
+    const inputString = params[1].split('');
+    const wordIndexes = [];
+    const modifiedAlphabet = this.rotateStringNspaces(ALPHABET, rowNumber);
+    inputString.forEach((element) => {
+      const index = modifiedAlphabet.indexOf(element);
+      if (index >= 0) {
+        wordIndexes.push(modifiedAlphabet.indexOf(element));
+      }
+    });
+
+    let originalMessage = '';
+
+    for (let j = 0; j < wordIndexes.length; j += 1) {
+      originalMessage += ALPHABET[wordIndexes[j]];
+    }
+    console.log(originalMessage);
     event.preventDefault();
+    this.setState({message: originalMessage});
+    return originalMessage;
+  }
+
+  rotateStringNspaces(alphabet, spaces) {
+    const sufix = alphabet.substring(0, spaces);
+    const prefix = alphabet.substring(spaces, alphabet.length);
+    return prefix + sufix;
   }
 }
 
