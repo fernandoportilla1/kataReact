3f21fefbe30aa608c29ebf555e00ed492ce249f4
Fernando Portilla <fernandoportilla1@gmail.com>
2019-08-31 16:32:56 -0500

    first Exercise1

--- src/Exercise1/Exercise1.js
+++ src/Exercise1/Exercise1.js
@@ -1,18 +1,40 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-export class Exercise1 extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
+const Exercise1 = () => {
+  const letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  let data = [];
+
+  for (let i = 0; i < letras.length; i++) {
+    data[i] = [];
+    for (let j = i; j < letras.length; j++) {
+      data[i].push(letras[j]);
+    }
+    for (let k = 0; k < i; k++) {
+      data[i].push(letras[k]);
+    }
   }
-  
-  render() {
-    return (
-      <div className="container">
-        Exercise1 page
-      </div>
-    );
-  }
-}
+
+  return (
+    <div className="container">
+      {
+        data.map(items => {
+          return (
+            <ul>
+              <li>
+                {
+                  items.map((item, key) => {
+                    return (
+                      <span key={key}>{item}</span>
+                    );
+                  })
+                }
+              </li>
+            </ul>
+          );
+        })
+      }
+    </div>
+  );
+};
 
 export default Exercise1;
