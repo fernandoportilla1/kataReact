bad6561879dfac58dcca20e3fa54004e61b16b4d
rojoyin <rojoyin@gmail.com>
2019-08-31 15:50:17 -0500

    Add input forms

--- src/Exercise2/Exercise2.js
+++ src/Exercise2/Exercise2.js
@@ -3,15 +3,29 @@ import React, {Component} from 'react';
 class Exercise2 extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {value: ''};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
   
   render() {
     return (
       <div className="container">
-        Exercise2 page
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     );
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    console.log('A name was submitted: ' + this.state.value);
+    event.preventDefault();
   }
 }
 
