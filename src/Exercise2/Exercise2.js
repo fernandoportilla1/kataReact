15b17d5ed52a3659ba0234e4e979adb85eee89ca
Fernando Portilla <fernandoportilla1@gmail.com>
2019-08-31 16:54:02 -0500

    second Exercise2

--- src/Exercise2/Exercise2.js
+++ src/Exercise2/Exercise2.js
@@ -1,18 +1,62 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-class Exercise2 extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
+const Exercise2 = () => {
+  const letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  let data = [];
+
+  for (let i = 0; i < letras.length; i++) {
+    data[i] = [];
+    for (let j = i; j < letras.length; j++) {
+      data[i].push(letras[j]);
+    }
+    for (let k = 0; k < i; k++) {
+      data[i].push(letras[k]);
+    }
   }
-  
-  render() {
-    return (
-      <div className="container">
-        Exercise2 page
-      </div>
-    );
-  }
-}
+
+  const palabra = 'POHAOB';
+  const palabra1 = 'VCZO';
+  const palabra2 = 'PCAPO';
+  const linea = 14;
+
+  let dataR = data[linea];
+  let res = '';
+
+  palabra.split('').forEach(letra => {
+    dataR.forEach((value, key) => {
+      if (letra === value) {
+        res = `${res}${data[0][key]}`;
+      }
+    });
+  });
+  console.log(res);
+  res = '';
+  palabra1.split('').forEach(letra => {
+    dataR.forEach((value, key) => {
+      if (letra === value) {
+        res = `${res}${data[0][key]}`;
+      }
+    });
+  });
+
+  console.log(res);
+  res = '';
+
+  palabra2.split('').forEach(letra => {
+    dataR.forEach((value, key) => {
+      if (letra === value) {
+        res = `${res}${data[0][key]}`;
+      }
+    });
+  });
+  console.log(res);
+
+
+  return (
+    <div className="container">
+      <a>sa</a>
+    </div>
+  );
+};
 
 export default Exercise2;
