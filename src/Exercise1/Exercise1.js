b35cba88c53ad829ec301dd152d55b40254a6f23
Fernando Portilla <fernandoportilla1@gmail.com>
2019-09-02 13:46:28 -0500

    draw answers

--- src/Exercise1/Exercise1.js
+++ src/Exercise1/Exercise1.js
@@ -1,37 +1,36 @@
 import React from 'react';
 
+import { useLetras } from '../Hooks/useLetras';
+
+const Dibujar = ({ items }) => {
+  return items.map((item, key) => {
+    return (
+      <span key={key}>{item}</span>
+    );
+  });
+};
+
 const Exercise1 = () => {
   const letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-  let data = [];
 
-  for (let i = 0; i < letras.length; i++) {
-    data[i] = [];
-    for (let j = i; j < letras.length; j++) {
-      data[i].push(letras[j]);
-    }
-    for (let k = 0; k < i; k++) {
-      data[i].push(letras[k]);
-    }
-  }
+  let { data, loading } = useLetras(letras, []);
 
   return (
     <div className="container">
       {
-        data.map(items => {
-          return (
-            <ul>
-              <li>
-                {
-                  items.map((item, key) => {
-                    return (
-                      <span key={key}>{item}</span>
-                    );
-                  })
-                }
-              </li>
-            </ul>
-          );
-        })
+        loading ? (<h1>Loading...</h1>) : (
+          <ul>
+            {
+              data.map((items, index) => {
+                return (
+                  <li key={index}>
+                    <Dibujar items={items} />
+                  </li>
+                );
+              })
+            }
+          </ul>
+        )
       }
     </div>
   );
--- src/Exercise2/Exercise2.js
+++ src/Exercise2/Exercise2.js
@@ -1,60 +1,50 @@
-import React from 'react';
+import React, { useRef } from 'react';
+
+import { useLetras } from '../Hooks/useLetras';
 
 const Exercise2 = () => {
+  const inputIndicador = useRef(null);
+  const inputClave = useRef(null);
+  const inputRespuesta = useRef(null);
   const letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-  let data = [];
 
-  for (let i = 0; i < letras.length; i++) {
-    data[i] = [];
-    for (let j = i; j < letras.length; j++) {
-      data[i].push(letras[j]);
-    }
-    for (let k = 0; k < i; k++) {
-      data[i].push(letras[k]);
-    }
-  }
+  let { data, loading } = useLetras(letras, []);
 
-  const palabra = 'POHAOB';
-  const palabra1 = 'VCZO';
-  const palabra2 = 'PCAPO';
-  const linea = 14;
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const indicador = inputIndicador.current.value;
+    const clave = inputClave.current.value;
 
-  let dataR = data[linea];
-  let res = '';
+    let dataR = data[indicador];
+    let respuesta = '';
 
-  palabra.split('').forEach(letra => {
-    dataR.forEach((value, key) => {
-      if (letra === value) {
-        res = `${res}${data[0][key]}`;
-      }
+    clave.toUpperCase().split('').forEach(letra => {
+      dataR.forEach((value, key) => {
+        if (letra === value) {
+          respuesta = `${respuesta}${data[0][key]}`;
+        }
+      });
     });
-  });
-  console.log(res);
-  res = '';
-  palabra1.split('').forEach(letra => {
-    dataR.forEach((value, key) => {
-      if (letra === value) {
-        res = `${res}${data[0][key]}`;
-      }
-    });
-  });
-
-  console.log(res);
-  res = '';
-
-  palabra2.split('').forEach(letra => {
-    dataR.forEach((value, key) => {
-      if (letra === value) {
-        res = `${res}${data[0][key]}`;
-      }
-    });
-  });
-  console.log(res);
-
+    inputRespuesta.current.value = respuesta;
+  };
 
   return (
     <div className="container">
-      <a>sa</a>
+      {
+        loading ? (<h1>Loading...</h1>) : (
+          <form onSubmit={handleSubmit}>
+            <label>Indicador</label>
+            <input type="number" ref={inputIndicador} placeholder="14" required />
+            <label>Clave</label>
+            <input type="text" ref={inputClave} placeholder="POHAOB" required />
+            <label>Respuesta</label>
+            <input type="text" ref={inputRespuesta} placeholder="...." disabled />
+            <button className="btn btn-primary" type="submit">
+              Decodificar
+            </button>
+          </form>
+        )
+      }
     </div>
   );
 };
